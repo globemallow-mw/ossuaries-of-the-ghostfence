389678d3bffba334697be6276f1f60a17821abde
JoanyMcKarelyn <109032964+JoanyMcKarelyn@users.noreply.github.com>
2023-01-02 15:24:16 +0800

    added an isModActive check in main.lua
    - added an isModActive check in main.lua to prevent the lua script from running without the plugin enabled which leads to issued like invalid item param provided

--- 01 Main Files (Required)/MWSE/mods/Ossuaries of the Ghostfence/main.lua
+++ 01 Main Files (Required)/MWSE/mods/Ossuaries of the Ghostfence/main.lua
@@ -1,5 +1,7 @@
-require("Ossuaries of the Ghostfence.Calm Ghosties")
--- require("Ossuaries of the Ghostfence.coc")
-require("Ossuaries of the Ghostfence.Lights Out")
-require("Ossuaries of the Ghostfence.SixthHouse")
-require("Ossuaries of the Ghostfence.DwemerTubeTooltip")
+if tes3.isModActive("Memento Mori.esp") then
+	require("Ossuaries of the Ghostfence.Calm Ghosties")
+	require("Ossuaries of the Ghostfence.Lights Out")
+	require("Ossuaries of the Ghostfence.SixthHouse")
+	require("Ossuaries of the Ghostfence.DwemerTubeTooltip")
+end
+
